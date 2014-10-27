46064c47b5d8899fb3a858b5db57e50af53bc807
mikekao <mike79212001.cs97@g2.nctu.edu.tw>
2014-10-27 19:07:43 +0800

    first version

--- test/index.php
+++ test/index.php
@@ -1,3 +1,19 @@
-<?php
-
-?>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+</head>
+	<script type="text/javascript" src="js/ajax.js"></script>
+<body>
+	
+	
+	
+	
+	
+	
+	
+	
+	<!-- jquery and bootstrap -->
+	<script src="js/jquery-1.11.0.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
